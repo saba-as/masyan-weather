6032e024c19921006b1c50b0d46c19bc2a3038b2
Saba Abdul-Aziz <sabawemp44@gmail.com>
2020-12-17 23:18:17 +0300

    f

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+let search;
+
+
+$("#search").click( () =>{
+
+    search = $("#city").val().toString();
+
+    getData(search)
+}
+
+   
+)
+
+
+
+
+
+function getData(search){
+
+    $.ajax({
+        url: 'http://api.weatherstack.com/current',
+        data: {
+          access_key: 'fd455105879e5b646cb86f08bb9a7254',
+          query: search,
+       
+        },
+        dataType: 'json',
+        success: function(apiResponse) {
+         
+
+            setData(apiResponse)
+
+          
+        }
+      });
+}
+
+
+function setData(data){
+
+    $("#title").text(data.location.country + ', ' + data.location.region)
+    $("#temp").text(data.current.temperature + '°C')
+    $("#wind_speed").text('wind speed ' + data.current.wind_speed + '    |    ')
+    $("#wind_degree").text('wind degree ' + data.current.wind_degree + '°' + '    |    '     )
+    $("#humidity").text('humidity ' + data.current.humidity + '%' )
+
+
+
+
+
+}
